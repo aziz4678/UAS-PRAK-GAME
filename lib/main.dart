1fd6a805d367faa5b0198c18ab2c58ed84624fea
aziz4678 <muhamadaziz564@gmail.com>
2022-12-13 18:53:48 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:api_app/screens/auth/auth_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+main() async {
+  runApp(GetMaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: AuthScreen(),
+    theme: ThemeData.dark(),
+  ));
+}
---
+++ lib/utils/api_endpoints.dart
@@ -0,0 +1,8 @@
+class ApiEndPoints {
+  static final String baseUrl = 'http://restapi.adequateshop.com/api/';
+  static _AuthEndPoints authEndpoints = _AuthEndPoints();
+}
+
+class _AuthEndPoints {
+  final String loginEmail = 'authaccount/login';
+}
